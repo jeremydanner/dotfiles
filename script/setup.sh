b4e0f035f6a7fa830bee9629f08ff90764642e5c
Jeremy Danner <jeremydanner2@gmail.com>
2018-06-03 10:11:37 +0200

    updades scripts

--- script/setup.sh
+++ script/setup.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
 # Set computer name
-sudo scutil --set ComputerName "Batman"
-sudo scutil --set HostName "Batman"
-sudo scutil --set LocalHostName "Batman"
+echo Set computer name?
+read computerName
+sudo scutil --set ComputerName $computerName
+sudo scutil --set HostName $computerName
+sudo scutil --set LocalHostName $computerName
+echo Your computer name is $computerName
 
 # Install homebrew
+echo Installing homebrew...
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
 
 # Install the latest version of Bash.
+echo Installing the latest version of Bash...
 brew install bash
 sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
 chsh -s /usr/local/bin/bash
 
 # install git, node and yarn
+echo install git, node, yarn and PHP
 brew install git
 brew install node
 brew install yarn
@@ -28,13 +34,12 @@ brew install homebrew/php/composer
 brew install homebrew/php/php-cs-fixer
 composer global install
 
-# https://github.com/FriendsOfPHP/PHP-CS-Fixer#globally-composer
-brew install homebrew/php/php-cs-fixer
 
 # Disable the Boot Sound on Startup (Mac)
 sudo nvram SystemAudioVolume=%80
 
 # Install macOS applications with Brew Cask.
+echo Install macOS applications with Brew Cask
 brew cask install 'hyper'
 brew cask install '1password'
 brew cask install 'appcleaner'
@@ -51,6 +56,7 @@ brew cask install 'android-studio'
 brew cask install 'microsoft-teams'
 brew cask install 'postman'
 
+echo Setting up system defaults
 # Save screenshots to the downloads directory
 defaults write com.apple.screencapture location -string "${HOME}/Downloads"
 
@@ -72,6 +78,7 @@ defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
 # Add the powerline fonts to make are terminal nice
+echo Adding the powerline fonts
 git clone https://github.com/powerline/fonts.git
 cd fonts
 ./install.sh
@@ -82,17 +89,15 @@ rm -rf fonts
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Move your files
-mkdir ~/Code
-cd Code
-git clone https://github.com/jeremydanner/dotfiles.git
-cd dotfiles
-cp .bash_profile ~/
-cp .bashrc ~/
-cp .hyper.js ~/
-cp .zshrc ~/
+echo moving bash files to the root
+cp ~/Code/dotfiles/.bash_profile ~/
+cp ~/Code/dotfiles/.bashrc ~/
+cp ~/Code/dotfiles/.hyper.js ~/
+cp ~/Code/dotfiles/.zshrc ~/
 
 
 # Laravel valet https://laravel.com/docs/5.4/valet
+echo installing Laravel valet
 composer global require laravel/valet
 valet install
 
