a838e4bf075e831148a55e2132a79740c374f66d
Jeremy Danner <jeremydanner2@gmail.com>
2018-06-10 14:12:57 +0200

    Update setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -34,8 +34,12 @@ brew install php
 sed -i".bak" "s/^\;phar.readonly.*$/phar.readonly = Off/g" /usr/local/etc/php/7.2/php.ini
 sed -i "s/memory_limit = .*/memory_limit = -1/" /usr/local/etc/php/7.2/php.ini
 
-brew install composer
-composer global install
+# Install composer 
+echo Intalling composer 
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
 
 
 # Disable the Boot Sound on Startup (Mac)
@@ -98,6 +102,8 @@ cp ~/Code/dotfiles/.bashrc ~/
 cp ~/Code/dotfiles/.hyper.js ~/
 cp ~/Code/dotfiles/.zshrc ~/
 
+# Make composer global
+mv ~/composer.phar /usr/local/bin/composer
 
 # Laravel valet https://laravel.com/docs/5.4/valet
 echo installing Laravel valet
