d1bea576ae565dd398c02f3239ebed01f0bd9eb9
Jeremy Danner <jeremydanner2@gmail.com>
2025-01-31 06:56:30 +0100

    remove pined formula

--- script/update.sh
+++ script/update.sh
@@ -3,12 +3,6 @@
 echo Looking for Homebrew updates
 brew update
 
-# 'brew unpin <formula>' To allow that formulae to update again
-# pin cask to ignor updates.
-echo Skipping upgrad of node
-node -v
-brew pin node@18 # node LTS
-
 echo Updating Taps
 brew upgrade
 
